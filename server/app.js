c948483e2d70ca0c0f360449af24621892d2d256
guptaaryan8092 <guptaaryan8092@gmail.com>
2025-08-28 16:05:20 +0530

    server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,11 @@ import {nanoid} from "nanoid"
 import connectDB from './src/config/mongo.config.js';
 import dotenv from 'dotenv';
 //Here {nanoid} is destructured with braces.
+import urlSchema from './src/models/short_url.model.js';
+import short_url from './src/routes/short_url.route.js';    
+import { redirectFromShortUrl } from './src/controller/short_url.controller.js';
+import { errorHandler } from './src/utils/errorHandler.js';
+
 
 dotenv.config();
 const app = express();
@@ -10,11 +15,10 @@ const app = express();
 app.use(express.json()) // Body parser
 app.use(express.urlencoded({extended:true}))
 
-app.post("/api/create",(req,res)=>{
-    const {url} = req.body
-    console.log(url)
-    res.send(nanoid(7))
-})
+app.use("/api/create",short_url)
+app.get("/:id",redirectFromShortUrl)
+
+app.use(errorHandler)
 
 const port = 5000;
 app.listen(port,()=>{
---
+++ server/src/dao/short_url.js
@@ -0,0 +1,23 @@
+import urlSchema from "../models/short_url.model.js";
+
+export const saveShortUrl = async (shortUrl, longUrl, userId) => {
+    try{
+        const newUrl = new urlSchema({
+            full_url:longUrl,
+            short_url:shortUrl
+        })
+        if(userId){
+            newUrl.user = userId
+        }
+        await newUrl.save()
+    }catch(err){
+        if(err.code == 11000){
+            throw new ConflictError("Short URL already exists")
+        }
+        throw new Error(err)
+    }
+};
+
+export const getShortUrl = async(shortUrl) =>{
+    return await urlSchema.findOneAndUpdate({short_url:shortUrl},{$inc:{clicks:1}})
+}
